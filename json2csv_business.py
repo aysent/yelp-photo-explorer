221134b178bc4106bc39c7eb3120395c27473963
Aysen Tatarinov <aysen.tatarinov@gmail.com>
2016-03-02 03:37:07 -0600

    Add business name to csv output

--- json2csv_business.py
+++ json2csv_business.py
@@ -3,16 +3,17 @@ import json
 def main():
 
     # print the header of output csv file
-    print 'business_id,city,latitude,longitude'
+    print 'business_id,name,city,latitude,longitude'
 
     # for each entry in input json file print one csv row
     for line in open("data/yelp_academic_dataset_business.json"):
         input_json = json.loads(line)
         business_id = input_json['business_id']
+        name = input_json['name'].encode('ascii', 'ignore').replace(',', '')
         city = input_json['city'].encode('ascii', 'ignore').replace(',', '')
         latitude = str(input_json['latitude'])
         longitude = str(input_json['longitude'])
-        print business_id + ',' + city + ',' + latitude + ',' + longitude
+        print business_id + ',' + name + ',' + city + ',' + latitude + ',' + longitude
 
 if __name__ == "__main__":
     main()
